53aaf45ee7b293499f096a5b2084833465f4c4df
Frederik Kristensen <frede4947@gmail.com>
2020-08-27 11:40:42 +0200

    Add project files.

---
+++ TestTCPserver/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TestTCPserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serverworker worker = new Serverworker();
+            worker.Start();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestTCPserver/Serverworker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TestTCPserver
+{
+    class Serverworker
+    {
+        public void Start()
+        {
+            // Her coder vi vores worker
+
+            // Vi opretter serveren
+            // Ip egen computer (altså 127.0.0.1), port er applikationen her har vi en ekko server så vi bruger port 7
+            TcpListener server = new TcpListener(IPAddress.Loopback, 7);
+            server.Start();
+
+            // Vent på en klient laver et opkald
+            TcpClient socket = server.AcceptTcpClient();
+
+            StreamReader sr = new StreamReader(socket.GetStream());
+            StreamWriter sw = new StreamWriter(socket.GetStream());
+
+            // Læses tekst fra client
+            String str = sr.ReadLine();
+            Console.WriteLine($"Her er server input: {str}");
+
+            // Svar clienten
+            sw.WriteLine(str);
+            sw.Flush(); // Tømmer buffer
+
+            socket.Close();
+
+        }
+    }
+}
